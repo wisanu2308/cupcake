77d78a2bf42f0acbacc8cbaaed469d91745dbd6f
Wisanu Kueansan <wisanu2308@gmail.om>
2018-10-15 10:09:19 +0700

    API sociablekit Tool

--- resources/views/igapi.blade.php
+++ resources/views/igapi.blade.php
@@ -6,5 +6,8 @@
 
 
 @section("htmlBody")
-IG
+
+<div class='sk-ww-instagram-hashtag-feed' data-embed-id='15136'></div>
+<script src='https://www.sociablekit.com/app/embed/instagram-hashtag-feed/widget.js'></script>
+
 @endsection
--- resources/views/twapi.blade.php
+++ resources/views/twapi.blade.php
@@ -1,1 +1,13 @@
 @extends("layout.layout")
+
+@section("stlye")
+
+@endsection
+
+
+@section("htmlBody")
+
+<div class='sk-ww-twitter-hashtag-feed' data-embed-id='15137'></div>
+<script src='https://www.sociablekit.com/app/embed/twitter-hashtag-feed/widget.js'></script>
+
+@endsection
