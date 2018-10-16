ca11928dda5e9e832cd28cfc9fbce8ce081a9ed8
wisanu2308 <wisanu2308@gmail.com>
2018-10-16 20:43:33 +0700

    twitter

--- resources/views/twapi.blade.php
+++ resources/views/twapi.blade.php
@@ -1,1 +1,34 @@
 @extends("layout.layout")
+
+
+@section("style")
+
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+	    
+	});
+
+</script>
+
+@endsection
+
+@section("htmlBody")
+
+<a class="twitter-hashtag-button" href="https://twitter.com/intent/tweet?button_hashtag=CakeBNK48" target="_blank">
+	Tweet
+</a>
+
+<br>
+
+<a class="twitter-grid" href="https://twitter.com/search?f=tweets&vertical=default&q=%23cakebnk48&src=typd">
+	National Park Tweets
+</a> 
+
+
+
+
+
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+@endsection
